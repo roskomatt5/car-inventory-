58f4cb3c1dabbaab3d983c088de159f9102ecddd
MatthewRosko <roskomatt5@gmail.com>
2024-01-18 13:58:58 -0600

    Fixing the two GETs

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,7 +5,7 @@ from models import db, User, Car, car_schema, cars_schema
 api = Blueprint('api',__name__, url_prefix='/api')
 
 
-@api.route('/car', methods = ['POST'])
+@api.route('/cars', methods = ['POST'])
 @token_required
 def create_car(current_user_token):
     make = request.json['make']
@@ -25,11 +25,11 @@ def create_car(current_user_token):
     return jsonify(response)
 
 
-@api.route('/car', methods = ['GET'])
+@api.route('/cars', methods = ['GET'])
 @token_required
 def get_car(current_user_token):
     a_user = current_user_token.token
-    car = car.query.filter_by(user_token = a_user).all()
+    car = Car.query.filter_by(user_token = a_user).all()
     response = cars_schema.dump(car)
     return jsonify(response)
 
